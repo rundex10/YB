c3aeedbf1cd49d67b20151286fcfba9f8f7a4000
rundex10 <129974375+rundex10@users.noreply.github.com>
2026-06-29 22:21:26 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,37 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyANm02Y1K4vzMRSYoohfNJbFJH9X4Z7Ua0",
-  authDomain: "you-bolt.firebaseapp.com",
-  projectId: "you-bolt",
-  storageBucket: "you-bolt.firebasestorage.app",
-  messagingSenderId: "640107402804",
-  appId: "1:640107402804:web:6b3284e1bb67b6b32adc61",
-  measurementId: "G-YZGEZS275C"
+
+    apiKey: "AIzaSyANm02Y1K4vzMRSYoohfNJbFJH9X4Z7Ua0",
+
+    authDomain: "you-bolt.firebaseapp.com",
+
+    projectId: "you-bolt",
+
+    storageBucket: "you-bolt.firebasestorage.app",
+
+    messagingSenderId: "640107402804",
+
+    appId: "1:640107402804:web:6b3284e1bb67b6b32adc61"
+
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+window.auth = auth;
+window.provider = provider;
+window.signInWithPopup = signInWithPopup;
+window.onAuthStateChanged = onAuthStateChanged;
+window.signOut = signOut;
